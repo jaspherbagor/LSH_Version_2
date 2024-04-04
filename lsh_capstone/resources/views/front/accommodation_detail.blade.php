33465e85bf1fa6770086c3a8e345170a56ec8ddc
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-04 09:18:59 +0800

    removed the commented codes

--- lsh_capstone/resources/views/front/accommodation_detail.blade.php
+++ lsh_capstone/resources/views/front/accommodation_detail.blade.php
@@ -30,23 +30,6 @@
                 </div>
             </div>
             @endforeach
-
-            {{-- <div class="col-md-3">
-                <div class="inner">
-                    <div class="photo">
-                        <img src="{{ asset('uploads/'.$item->featured_photo) }}" alt="">
-                        <img src="{{ asset('uploads/n4.jpg') }}" alt="">
-                    </div>
-                    <div class="text">
-                        <h2><a href="{{ route('room_detail',$item->id) }}">{{ $item->name }}</a></h2>
-                        <h2 class="text-center">Casie Hotel</h2>
-                        <div class="button">
-                            <a href="{{ route('room_detail',$item->id) }}" class="btn btn-primary">See Detail</a>
-                            <a href="#" class="btn btn-primary">See Accommodation</a>
-                        </div>
-                    </div>
-                </div>
-            </div> --}}
         </div>
     </div>
 </div>
