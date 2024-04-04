98b6de53eac53d58ab880b2cbecf4bd1967c8f2c
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-05 04:33:49 +0800

    added changes to the accommodation detail page

--- lsh_capstone/resources/views/front/accommodation_detail.blade.php
+++ lsh_capstone/resources/views/front/accommodation_detail.blade.php
@@ -24,7 +24,7 @@
                     <div class="text">
                         <h2><a href="{{ route('room_detail',$item->id) }}">{{ $item->name }}</a></h2>
                         <div class="button">
-                            <a href="{{ route('room_detail',$item->id) }}" class="btn btn-primary">See Rooms</a>
+                            <a href="{{ route('room_detail',$item->id) }}" class="btn btn-primary">See Detail</a>
                         </div>
                     </div>
                 </div>
