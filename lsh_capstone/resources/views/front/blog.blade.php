62404d0ba9b8bc7bff5b8b2518c3631bc24eda2e
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-05 18:51:59 +0800

    added the elements of the blog page in the card

--- lsh_capstone/resources/views/front/blog.blade.php
+++ lsh_capstone/resources/views/front/blog.blade.php
@@ -18,18 +18,20 @@
             @foreach($post_all as $item)
             <div class="col-md-4">
                 <div class="inner">
-                    <div class="photo">
-                        <img src="{{ asset('uploads/'.$item->photo) }}" alt="">
-                    </div>
-                    <div class="text">
-                        <h2><a href="{{ route('single_post', $item->id) }}">{{ $item->heading }}</a></h2>
-                        <div class="short-des">
-                            <p>
-                                {!! $item->short_content !!}
-                            </p>
+                    <div class="card blog-card">
+                        <div class="photo card-photo">
+                            <img src="{{ asset('uploads/'.$item->photo) }}" alt="" class="img-fluid">
                         </div>
-                        <div class="button">
-                            <a href="{{ route('single_post', $item->id) }}" class="btn btn-primary">Read More</a>
+                        <div class="text card-body">
+                            <h2><a href="{{ route('single_post', $item->id) }}">{{ $item->heading }}</a></h2>
+                            <div class="short-des">
+                                <p>
+                                    {!! $item->short_content !!}
+                                </p>
+                            </div>
+                            <div class="button">
+                                <a href="{{ route('single_post', $item->id) }}" class="btn btn-primary">Read More</a>
+                            </div>
                         </div>
                     </div>
                 </div>
