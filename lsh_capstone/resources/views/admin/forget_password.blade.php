f9be7e4c25affecfba86393fefea2237fc168a17
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-06 18:44:33 +0800

    updated the admin forgot password page

--- lsh_capstone/resources/views/admin/forget_password.blade.php
+++ lsh_capstone/resources/views/admin/forget_password.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
 
-    <link rel="icon" type="image/png" href="uploads/favicon.png">
+    <link rel="icon" type="image/png" href="{{ asset('uploads/lsh_favicon_admin.svg') }}">
 
-    <title>Admin Panel</title>
+    <title>Admin Forgot Password</title>
 
     <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@400;600;700&display=swap" rel="stylesheet">
 
@@ -22,7 +22,7 @@
             <div class="container container-login">
                 <div class="row">
                     <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">
-                        <div class="card card-primary border-box">
+                        <div class="card card-primary border-box forgot-password-card">
                             <div class="card-header card-header-auth">
                                 <h4 class="text-center">Reset Password</h4>
                             </div>
