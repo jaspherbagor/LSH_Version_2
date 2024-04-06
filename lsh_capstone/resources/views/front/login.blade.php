baddd1e6593818d04086d025fbc0cf4e9618d777
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-06 20:37:42 +0800

    added a style to the login page in the front-end

--- lsh_capstone/resources/views/front/login.blade.php
+++ lsh_capstone/resources/views/front/login.blade.php
@@ -1,55 +1,93 @@
-@extends('front.layout.app')
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
+        <meta name="description" content="">
+        <title>Labason Safe Haven Login</title>        
+		
+        <link rel="icon" type="image/png" href="{{ asset('uploads/'.$global_setting_data->favicon) }}">
 
-@section('main_content')
-<div class="page-top">
-    <div class="bg"></div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-12">
-                <h2>{{ $global_page_data->signin_heading }}</h2>
-            </div>
-        </div>
-    </div>
-</div>
+        @include('front.layout.styles')
+        @include('front.layout.scripts')
 
-<div class="page-content">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-4">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 
-                <form action="{{ route('customer_login_submit') }}" method="post">
-                    @csrf
-                    <div class="login-form">
-                        <div class="mb-3">
-                            <label for="" class="form-label">Email Address</label>
-                            <input type="text" class="form-control" name="email">
-                            @if($errors->has('email'))
-                                <span class="text-danger">{{ $errors->first('email') }}</span>
-                            @endif
+        {{-- Oswald Google Font Link CDN --}}
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+
+        {{-- Montserrat Font --}}
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    </head>
+    <body class="login-body-container px-2">
+
+        
+        <div class="page-content">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-4 py-4 px-4 login-container">
+                        <div class="text-center">
+                            <a href="{{ route('home') }}">
+                                <img src="{{ asset('uploads/logo.png') }}" alt="" class="logo">
+                            </a>
                         </div>
-                        <div class="mb-3">
-                            <label for="" class="form-label">Password</label>
-                            <input type="password" class="form-control" name="password">
-                            @if($errors->has('password'))
-                                <span class="text-danger">{{ $errors->first('password') }}</span>
-                            @endif
-                        </div>
-                        <div class="mb-3">
-                            <button type="submit" class="btn btn-primary bg-website">Login</button>
-                            
-                        </div>
-                        <div class="mb-3">
-                            <a href="{{ route('customer_forget_password') }}" class="primary-color">Forget Password?</a>
-                            <br>
-                            Don't have an account yet?
-                            <a href="{{ route('customer_signup') }}" class="primary-color">Register instead.</a>
-                        </div>
+                        <h2 class="text-center mb-4 mt-3">Login Account</h2>     
+                        <form action="{{ route('customer_login_submit') }}" method="post">
+                            @csrf
+                            <div class="login-form">
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Email Address</label>
+                                    <input type="text" class="form-control form-input" name="email">
+                                    @if($errors->has('email'))
+                                        <span class="text-danger">{{ $errors->first('email') }}</span>
+                                    @endif
+                                </div>
+                                <div class="mb-3">
+                                    <label for="" class="form-label">Password</label>
+                                    <input type="password" class="form-control form-input" name="password">
+                                    @if($errors->has('password'))
+                                        <span class="text-danger">{{ $errors->first('password') }}</span>
+                                    @endif
+                                </div>
+                                <div class="mb-4 text-center">
+                                    <button type="submit" class="btn btn-primary bg-website">Login</button>
+                                    
+                                </div>
+                                <div class="mt-2">
+                                    <a href="{{ route('customer_forget_password') }}" class="primary-color">Forget password.</a>
+                                    <br>
+                                    Don't have an account yet?
+                                    <a href="{{ route('customer_signup') }}" class="primary-color">Register instead.</a>
+                                </div>
+                            </div>
+                        </form>
+        
+        
                     </div>
-                </form>
-
-
+                </div>
             </div>
         </div>
-    </div>
-</div>
-@endsection
+		
+        @if(session()->get('error'))
+        <script>
+        iziToast.error({
+            title: '',
+            position: 'topRight',
+            message: '{{ session()->get('error') }}',
+        });
+        </script>
+        @endif
+        @if(session()->get('success'))
+        <script>
+        iziToast.success({
+            title: '',
+            position: 'topRight',
+            message: '{{ session()->get('success') }}',
+        });
+        </script>
+        @endif
+   </body>
+</html>
