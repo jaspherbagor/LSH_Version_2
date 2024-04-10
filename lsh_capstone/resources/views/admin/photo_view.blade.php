170026c1f88d3b06a631147563195d83d2d875f5
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-10 20:05:18 +0800

    added a tooltip and convert the button text in the photo view page in the admin panel

--- lsh_capstone/resources/views/admin/photo_view.blade.php
+++ lsh_capstone/resources/views/admin/photo_view.blade.php
@@ -28,8 +28,12 @@
                                         <td><img src="{{ asset('uploads/'.$row->photo) }}" alt="slide_image" class="w_200"></td>
                                         <td>{{ $row->caption }}</td>
                                         <td class="pt_10 pb_10">
-                                            <a href="{{ route('admin_photo_edit',$row->id) }}" class="btn btn-primary mb-1">Edit</a>
-                                            <a href="{{ route('admin_photo_delete',$row->id) }}" class="btn btn-danger mb-1" onClick="return confirm('Are you sure you want to delete this photo?');">Delete</a>
+                                            <a href="{{ route('admin_photo_edit',$row->id) }}" class="btn btn-primary mb-1" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                <i class="fa fa-pencil" aria-hidden="true"></i>
+                                            </a>
+                                            <a href="{{ route('admin_photo_delete',$row->id) }}" class="btn btn-danger mb-1" onClick="return confirm('Are you sure you want to delete this photo?');" data-toggle="tooltip" data-placement="top" title="Delete">
+                                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                            </a>
                                         </td>
                                         
                                     </tr>
