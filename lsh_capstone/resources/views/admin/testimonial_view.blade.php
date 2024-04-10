7ca30e12c94f8ca84a884f6c3639c7df3f7db512
Jaspher Bagor <jaspherbagor0357@gmail.com>
2024-04-10 19:58:53 +0800

    added tooltip and convert the button texts into icons in the testimonial page in the admin panel

--- lsh_capstone/resources/views/admin/testimonial_view.blade.php
+++ lsh_capstone/resources/views/admin/testimonial_view.blade.php
@@ -30,8 +30,12 @@
                                         <td>{{ $row->name }}</td>
                                         <td>{{ $row->designation }}</td>
                                         <td class="pt_10 pb_10">
-                                            <a href="{{ route('admin_testimonial_edit',$row->id) }}" class="btn btn-primary">Edit</a>
-                                            <a href="{{ route('admin_testimonial_delete',$row->id) }}" class="btn btn-danger" onClick="return confirm('Are you sure?');">Delete</a>
+                                            <a href="{{ route('admin_testimonial_edit',$row->id) }}" class="btn btn-primary" data-toggle="tooltip" data-placement="top" title="Edit">
+                                                <i class="fa fa-pencil" aria-hidden="true"></i>
+                                            </a>
+                                            <a href="{{ route('admin_testimonial_delete',$row->id) }}" class="btn btn-danger" onClick="return confirm('Are you sure?');" data-toggle="tooltip" data-placement="top" title="Delete">
+                                                <i class="fa fa-trash-o" aria-hidden="true"></i>
+                                            </a>
                                         </td>
                                     </tr>
                                     @endforeach
